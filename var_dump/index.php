1c1f7238dfd61d0a5aeed74d3af5b07fb26bf7a4
mehedihasanhridoy <mehedihasanhridoy543@gmail.com>
2022-02-21 14:06:21 +0600

    php var_dump done

---
+++ var_dump/index.php
@@ -0,0 +1,35 @@
+<?php
+
+//var_dump() holo variable check function
+//var_dump() check kora ja variable a ki type ar data asa 
+//var_dump() holo similar of javascript function type of()
+
+$name = "mehedi hasan hridoy";//string
+var_dump($name);
+
+echo "<br>";
+
+$num = 520;//integer
+var_dump($num);
+
+echo "<br>";
+
+$num2 = 50.80;//float
+var_dump($num2);
+
+echo "<br>";
+
+$admin = true;//boolen
+var_dump($admin);
+
+echo "<br>";
+
+$Array = ["apple","banana","mango","orange"];//array
+var_dump($Array);
+
+echo "<br>";
+
+define("SCHOOL", "Feni Pilot High School");//constant
+var_dump(SCHOOL);
+
+?>
