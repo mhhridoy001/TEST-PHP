5382eca5c6984993a121f647260b7fbd6700b3ec
mehedihasanhridoy <mehedihasanhridoy543@gmail.com>
2022-02-21 14:04:48 +0600

    php Echo & print done

---
+++ Echo_Print/index.php
@@ -0,0 +1,31 @@
+<?php
+
+	//print & echo different
+	echo "Assalamualaikum <br>";
+
+	print("mehedi hasan hridoy <br>");
+
+	$test= print("Hello");//print default 1 value store kora 
+	echo "<br>";
+	print   $test."<br>";
+
+	echo $test="Hello";//echo default value store kora na
+	echo "<br>";
+	echo $test."<br>";
+
+	//print "Allahu","AKBAR"; //print maltipul value nia kaj korta para nah
+
+	echo "apple"," orange"," banana";
+
+	echo "<h1>mehedi \"hasan\" hridoy</h1>"; //echo in using scap sequance
+
+	$name= "mehedi";
+	$amount= 500;
+
+	echo "My name is:". $name ."<br>"."My amount is:". $amount; //concatination
+	echo "<br>";
+	echo "My name is:$name  ,   My amount is: $amount"; // easy way
+
+	echo "<br>";
+
+?>
